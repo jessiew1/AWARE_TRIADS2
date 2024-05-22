0f1c444e5552f9d56ed698ce6eee9858e81482a9
jessiew1 <123497382+jessiew1@users.noreply.github.com>
2024-05-22 16:37:36 -0500

    Add files via upload

--- RandomFile.swift
+++ RandomFile.swift
@@ -1,10 +1,14 @@
 import Foundation
+import AWAREFramework
+import MySQLNIO
 import UserNotifications
 
-// Extend the existing NotificationManager class with new functionality
-extension NotificationManager {
+class NotificationManager {
+    static let shared = NotificationManager()
+    let device_id = AWAREStudy.shared().getDeviceId()
+    let device_name = AWAREStudy.shared().getDeviceName()
+    private var notificationCount = 0
 
-    // This function schedules random notifications for a specified number of days
     func scheduleRandomNotificationsForMultipleDays(numberOfDays: Int) {
         let timeRanges = [
             (startHour: 9, startMinute: 0, endHour: 13, endMinute: 0),
@@ -18,40 +22,38 @@ extension NotificationManager {
             }
         }
     }
+
     
-    // This function schedules one random notification between the given start and end times, considering the day offset
+    
+    
     private func scheduleRandomNotificationBetween(startHour: Int, startMinute: Int, endHour: Int, endMinute: Int, dayOffset: Int) {
         var dateComponents = Calendar.current.dateComponents([.year, .month, .day], from: Date())
         dateComponents.day! += dayOffset
-        
+
         var startDateComponents = dateComponents
         startDateComponents.hour = startHour
         startDateComponents.minute = startMinute
-        
+
         var endDateComponents = startDateComponents
         endDateComponents.hour = endHour
         endDateComponents.minute = endMinute
-        
+
         guard let startTime = Calendar.current.date(from: startDateComponents),
               let endTime = Calendar.current.date(from: endDateComponents) else { return }
-        
+
         let randomTimeInterval = TimeInterval(arc4random_uniform(UInt32(endTime.timeIntervalSince(startTime))))
         let randomDate = startTime.addingTimeInterval(randomTimeInterval)
-        
+
         scheduleNotification(for: randomDate)
         var datesForNotification = [Date]()
         datesForNotification.append(randomDate)
-        // Assuming randomDate is an array of Date objects
         scheduleImmediateNotification(for: datesForNotification)
     }
-    
-    
-    
+
     func scheduleImmediateNotification(for dates: [Date]) {
         let content = UNMutableNotificationContent()
         content.title = "Random Survey Schedule"
 
-        // Formatting the dates into a string
         let dateFormatter = DateFormatter()
         dateFormatter.dateStyle = .medium
         dateFormatter.timeStyle = .short
@@ -60,37 +62,84 @@ extension NotificationManager {
         content.body = "Notifications scheduled for: \(dateString)"
         content.sound = .default
 
-        // Triggering the notification immediately
         let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 1, repeats: false)
 
+        //let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+        //UNUserNotificationCenter.current().add(request) { error in
+           // if let error = error {
+            //    print("Error scheduling immediate notification: \(error)")
+           // }
+       // }
+    }
+
+    func scheduleAndStoreNotification(for date: Date, content: UNMutableNotificationContent, device_id: String, device_name: String) {
+        let triggerDate = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute], from: date)
+        let trigger = UNCalendarNotificationTrigger(dateMatching: triggerDate, repeats: false)
         let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+
         UNUserNotificationCenter.current().add(request) { error in
             if let error = error {
-                print("Error scheduling immediate notification: \(error)")
+                print("Error scheduling notification: \(error)")
+            } else {
+                //self.storeNotificationDetailsInDatabase(message_id: request.identifier, date: date, message_text: content.body, device_id: device_id, device_name: device_name)
             }
         }
     }
 
-    
-    
-    
-    // This function schedules a notification for a given date
+ func storeNotificationDetailsInDatabase(message_id: String, date: Date, message_text: String, device_id: String, device_name: String) {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        let dateString = dateFormatter.string(from: date)
+
+        let sql = "INSERT INTO Notifications (message_id, date, message_text, device_id, device_name) VALUES (?, ?, ?, ?, ?)"
+
+        let values: [MySQLData] = [
+            MySQLData(string: message_id),
+            MySQLData(string: dateString),
+            MySQLData(string: message_text),
+            MySQLData(string: device_id),
+            MySQLData(string: device_name)
+        ]
+
+        MySQLConnectionHandler.shared.pool?.withConnection { connection in
+            connection.query(sql, values).map { rows in
+                print("Notification stored successfully.")
+            }.flatMapError { error in
+                print("Failed to store notification: \(error)")
+                return connection.eventLoop.makeFailedFuture(error)
+            }
+        }
+    }
+
     private func scheduleNotification(for date: Date) {
         let content = UNMutableNotificationContent()
-        content.title = "Random Survey Time"
-        content.body = "It's time for a random survey! Tap to participate."
+        content.title = "Random Survey"
+        content.body = "Tap to participate!"
         content.sound = .default
         content.userInfo = ["deep_link_url": "https://wustl.az1.qualtrics.com/jfe/form/SV_0HyB20WVoAztGTk"]
-        
+
         let triggerDate = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute], from: date)
         let trigger = UNCalendarNotificationTrigger(dateMatching: triggerDate, repeats: false)
-        
         let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
-        
+
         UNUserNotificationCenter.current().add(request) { error in
             if let error = error {
                 print("Error scheduling random notification: \(error)")
+            } else {
+                self.notificationCount += 1
+                print(self.notificationCount)
+                if self.notificationCount > 21 {
+                    //self.storeNotificationDetailsInDatabase(message_id: request.identifier, date: date, message_text: content.body, device_id: self.device_id, device_name: self.device_name)
+                }
             }
         }
+    }
+
+    func scheduleTwoStartupNotifications() {
+        let firstNotificationDate = Date().addingTimeInterval(60) // 1 minute after app start
+        let secondNotificationDate = Date().addingTimeInterval(120) // 2 minutes after app start
+
+        scheduleNotification(for: firstNotificationDate)
+        scheduleNotification(for: secondNotificationDate)
     }
 }
